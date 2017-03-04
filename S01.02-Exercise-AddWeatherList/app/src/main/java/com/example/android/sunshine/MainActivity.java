a6816834a416bcfc36541c61ecbeff82e2ee6c9d
Frederico Teixeira <mrfrederico89@gmail.com>
2017-03-04 20:27:37 +0000

    Add a better fake weather data

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -33,11 +33,26 @@ public class MainActivity extends AppCompatActivity {
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
         // COMPLETED (3) Create an array of Strings that contain fake weather data
-        String[] fakeWeatherArray = {"Cloud","Storm", "Rain", "Snow"};
+        String[] dummyWeatherData = {
+                "Today, May 17 - Clear - 17°C / 15°C",
+                "Tomorrow - Cloudy - 19°C / 15°C",
+                "Thursday - Rainy- 30°C / 11°C",
+                "Friday - Thunderstorms - 21°C / 9°C",
+                "Saturday - Thunderstorms - 16°C / 7°C",
+                "Sunday - Rainy - 16°C / 8°C",
+                "Monday - Partly Cloudy - 15°C / 10°C",
+                "Tue, May 24 - Meatballs - 16°C / 18°C",
+                "Wed, May 25 - Cloudy - 19°C / 15°C",
+                "Thu, May 26 - Stormy - 30°C / 11°C",
+                "Fri, May 27 - Hurricane - 21°C / 9°C",
+                "Sat, May 28 - Meteors - 16°C / 7°C",
+                "Sun, May 29 - Apocalypse - 16°C / 8°C",
+                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
+        };
 
         // COMPLETED (4) Append each String from the fake weather data array to the TextView
-        for (String fakeWeather : fakeWeatherArray) {
-            mWeatherTextView.append(fakeWeather + "\n\n\n");
+        for (String dummyWeather : dummyWeatherData) {
+            mWeatherTextView.append(dummyWeather + "\n\n\n");
         }
     }
 }
